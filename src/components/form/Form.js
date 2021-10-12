ad67ac17efe3d4345845b03257355454a84788fc
Ilya Gonchar <i.o.gonchar@yandex.ru>
2021-10-12 17:08:45 +0300

    fix: form title shows if needed

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -6,7 +6,7 @@ const Form = ({ title, buttonText, onSubmit, isDisabled, children }) => {
 
   return (
     <div className="form-container">
-      <h2 className="form-container__title">{ title }</h2>
+      { title && <h2 className="form-container__title">{ title }</h2> }
       <form className="form" onSubmit={onSubmit} noValidate>
         { children }
         <button className="form__submit-btn" disabled={isDisabled}>{ buttonText }</button>
