8df6688f92de716ebfa186ca9be8c57c270d5ab3
Ilya Gonchar <i.o.gonchar@yandex.ru>
2021-10-12 17:05:43 +0300

    feat: saga types for hotels and favourites added

--- src/store/types.js
+++ src/store/types.js
@@ -2,6 +2,10 @@
 export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
+export const HOTELS_REQUEST = 'HOTELS_REQUEST';
+export const ADD_TO_FAVOURITES = 'ADD_TO_FAVOURITES';
+export const REMOVE_FROM_FAVOURITES = 'REMOVE_FROM_FAVOURITES';
+export const SORT_FAVOURITES = 'SORT_FAVOURITES';
 
 // redux types
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
