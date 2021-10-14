bf4613212e47730f25f64f117f3c12c4dad414fe
Ilya Gonchar <i.o.gonchar@yandex.ru>
2021-10-14 16:23:18 +0300

    heroku: test gethotels

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,8 @@
 export const getHotels = (location, checkInDate, checkOutDate) => {
-  console.log(location, checkInDate, checkOutDate);
   return fetch(`https://engine.hotellook.com/api/v2/cache.json?location=${location}&currency=rub&checkIn=${checkInDate}&checkOut=${checkOutDate}&limit=50`, {
     method: 'GET',
   })
   .then(res => {
-    console.log(res)
     if (res.ok) {
       return res.json();
     }
